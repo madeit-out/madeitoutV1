a89b13fb14e5d6fc30be311bb2b7c3658fe7217b
Avionte Williams <aviontewilliams@gmail.com>
2025-12-05 12:32:09 -0500

    host for deployment

--- App/Backend/run.py
+++ App/Backend/run.py
@@ -2,11 +2,15 @@
 # import gevent.monkey
 # gevent.monkey.patch_all(ssl=False)
 
+import os
 from App import create_app
 
-# app, socketio_instance = create_app()
+app, socketio_instance = create_app()
 
-if __name__ == '__main__':
-    app, socketio_instance = create_app()
-    socketio_instance.run(app, debug=True, host='127.0.0.1', port=5000)
-    
+if __name__ == "__main__":
+    socketio_instance.run(
+        app,
+        host="0.0.0.0",                                 # required for Render
+        port=int(os.environ.get("PORT", 5000)),        # Render gives you a PORT env var
+        allow_unsafe_werkzeug=True                     # allow Werkzeug in production
+    )
